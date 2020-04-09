effb9a194e08b272a5e71d4a4bd9a1bbfc809f87
rogman <rogman@0.hsd1.ca.comcast.net>
2020-04-09 13:42:46 -0700

    first commit

---
+++ main.tf
@@ -0,0 +1,61 @@
+terraform {
+  required_version = ">= 0.11.0"
+}
+
+data "aws_ami" "rhel_ami" {
+  most_recent = true
+  owners      = ["309956199498"]
+
+  filter {
+    name   = "name"
+    values = ["*RHEL-7.3_HVM_GA-*"]
+  }
+}
+
+provider "aws" {
+  region  = var.aws_region
+  profile = var.aws_profile
+}
+
+resource "random_id" "name" {
+  byte_length = 4
+}
+
+resource "aws_key_pair" "awskey" {
+  key_name   = "awskwy-${random_id.name.hex}"
+  public_key = tls_private_key.awskey.public_key_openssh
+}
+
+resource "aws_security_group" "allow_all" {
+  name        = "allow-all-${random_id.name.hex}"
+  description = "Allow all inbound traffic"
+
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "ubuntu" {
+  ami               = data.aws_ami.rhel_ami.id
+  instance_type     = var.instance_type
+  availability_zone = "${var.aws_region}a"
+  key_name          = aws_key_pair.awskey.key_name
+  security_groups   = [aws_security_group.allow_all.name]
+
+  tags = {
+    Name        = var.name
+    TTL         = var.ttl
+    Owner       = var.owner
+    Description = "This branch updated v5"
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,7 @@
+#Instance
+variable "instance_type" {}
+variable "name" {}
+variable "owner" {}
+variable "ttl" {}
+variable "aws_profile" {}
+variable "aws_region" {}
