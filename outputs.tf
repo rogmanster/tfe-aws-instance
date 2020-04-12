9bafc3aeda0a8200709d9e29595f8641112401b0
rogman <rogman@0.local>
2020-04-12 16:29:37 -0700

    fix

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,3 @@
-output "public_dns" {
-  value = aws_instance.ubuntu.public_dns
-}
-
 output "public_ip" {
   value = aws_instance.ubuntu.public_ip
 }
@@ -9,4 +5,3 @@ output "public_ip" {
 output "private_key_pem" {
   value = tls_private_key.awskey.private_key_pem
 }
-
