a69f46e01dd78a91d709be1e65e8e8d0b0fe7c3c
Roger Chao <rogman@gmail.com>
2020-04-12 02:32:37 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -24,30 +24,24 @@ resource "aws_key_pair" "awskey" {
   public_key = tls_private_key.awskey.public_key_openssh
 }
 
-resource "aws_security_group" "allow_all" {
-  name        = "allow-all-${random_id.name.hex}"
-  description = "Allow all inbound traffic"
+// Workspace Data
+data "terraform_remote_state" "aws_security_group" {
+  backend = "remote"
 
-  ingress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+  config = {
+   organization = "rogercorp"
+   workspaces = {
+     name = "aws-security-group"
+   }
   }
 }
+
 
 resource "aws_instance" "ubuntu" {
   ami               = data.aws_ami.rhel_ami.id
   instance_type     = var.instance_type
   key_name          = aws_key_pair.awskey.key_name
-  security_groups   = [aws_security_group.allow_all.name]
+  security_groups   = data.terraform_remote_state.aws_security_group.security_group_id
 
   tags = {
     Name        = var.name
