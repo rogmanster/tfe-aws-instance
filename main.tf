d2dd8aae599726447c73f0dab17288c2667c8a29
rogman <rogman@0.local>
2020-04-12 16:19:07 -0700

    fix

--- main.tf
+++ main.tf
@@ -2,6 +2,29 @@ terraform {
   required_version = ">= 0.11.0"
 }
 
+// Workspace Data
+data "terraform_remote_state" "rogercorp_aws_vpc_prod" {
+  backend = "remote"
+
+  config = {
+    organization = "rogercorp"
+    workspaces = {
+      name = "aws-vpc-prod"
+    }
+}
+
+// Workspace Data
+data "terraform_remote_state" "aws_security_group" {
+  backend = "remote"
+
+  config = {
+    organization = "rogercorp"
+    workspaces = {
+      name = "aws-security-group-prod"
+    }
+  }
+}
+
 data "aws_ami" "rhel_ami" {
   most_recent = true
   owners      = ["309956199498"]
@@ -24,17 +47,8 @@ resource "aws_key_pair" "awskey" {
   public_key = tls_private_key.awskey.public_key_openssh
 }
 
-// Workspace Data
-data "terraform_remote_state" "aws_security_group" {
-  backend = "remote"
 
-  config = {
-   organization = "rogercorp"
-   workspaces = {
-     name = "aws-security-group-prod"
-   }
-  }
-}
+
 
 resource "aws_instance" "ubuntu" {
   ami                     = data.aws_ami.rhel_ami.id
