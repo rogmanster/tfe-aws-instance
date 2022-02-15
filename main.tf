dfe1509323cfc795743d4d61d259a50fadd4e225
rogmanster <rogman@gmail.com>
2022-02-15 10:09:40 -0800

    add v15

--- main.tf
+++ main.tf
@@ -79,6 +79,6 @@ resource "aws_instance" "ubuntu" {
     Name        = var.name
     TTL         = var.ttl
     Owner       = var.owner
-    Description = "This branch updated v14"
+    Description = "This branch updated v15"
   }
 }
