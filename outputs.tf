a767bde43a8ffe1aba21d7aae6d216da9184a9ba
rogmanster <rogman@gmail.com>
2022-11-07 10:18:39 -0800

    remove ssh key

--- outputs.tf
+++ outputs.tf
@@ -2,7 +2,7 @@ output "public_ip" {
   value = aws_instance.ubuntu.*.public_ip
 }
 
-output "private_key_pem" {
-  value = tls_private_key.awskey.*.private_key_pem
-  sensitive = true
-}
+#output "private_key_pem" {
+#  value = tls_private_key.awskey.*.private_key_pem
+#  sensitive = true
+#}
